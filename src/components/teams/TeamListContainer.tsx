f7274233d4f83d3209aa6af4f6da8e920713874e
ViMonks <60519132+ViMonks@users.noreply.github.com>
2021-01-18 13:41:38 -0500

    TeamListContainer cleaned up.

--- src/components/teams/TeamListContainer.tsx
+++ src/components/teams/TeamListContainer.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import teamList from '../../fakeAPI/teamList';
 import toast from 'react-hot-toast';
 import { useQuery } from 'react-query';
-import { useAuth } from '../context/AuthContext';
 
 // interfaces
-import { Team, NewTeamProps } from '../../types';
+import { NewTeamProps } from '../../types';
 
 // internal imports
 import TeamListView from './TeamListView';
 import CreateTeamModalForm from './CreateTeamModalForm';
+import { useAuth } from '../context/AuthContext';
 import { getTeamsList } from '../API/Api';
 import LoadingBar from '../LoadingBar';
 
@@ -18,19 +17,12 @@ const TeamListContainer = (): React.ReactElement => {
     const { data: user } = useQuery('user', async () => await currentUser.getIdToken());
     const { isLoading, error, data } = useQuery<any, Error>(['team', user], () => getTeamsList(user), { enabled: !!user });
 
-    const getTeams = (): Team[] => {
-        // TODO: this is where the API call to get a user's teams will live
-        return teamList;
-    };
-
     const createTeam = (newTeam: NewTeamProps): void => {
         // TODO: This is where the API call to submit a new team POST request will live
         console.log('New Team');
         console.log(newTeam);
         toast.success('New team created!');
     };
-
-    const [teams, setTeams] = React.useState(getTeams());
 
     return (
         <div className="container">
